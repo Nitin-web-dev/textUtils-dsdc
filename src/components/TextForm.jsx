b5b3f6254acc357adfc24a432930304b12e31647
Nitin Singh <Nitinwebdev5@gmail.com>
2023-04-06 10:24:47 +0530

    clear btn add or summary and textWordCout

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -3,7 +3,7 @@ import React,{useState} from 'react'
 export default function TextForm() {
 
   // destructuring ;
-    const [ text, setText] = useState("Enter YOU text here");
+    const [ text, setText] = useState("");
 
     const handleChange = (e) =>{
       let newText = e.target.value;
@@ -21,6 +21,10 @@ export default function TextForm() {
     
     }
 
+    const handleClearText = (e) => {
+      setText("");
+    }
+
   return (
     <div>
   <div className=" mb-3">
@@ -36,7 +40,18 @@ export default function TextForm() {
   </div>
     <button className='btn btn-primary mx-2'  onClick={handleUpperCase}>Change to UpperCase</button>
     <button className='btn btn-primary mx-2' onClick={handleLowerCase}>Change to lowerCase</button>
+    <button className='btn btn-outline-danger mx-2' onClick={handleClearText}>Clear</button>
+    <hr /> 
+      <div className='p-2'>
+        <h1>Your Text Summery</h1>
+      </div>
 
+      <p>Your Text Contains : {text.split(" ").length-1} words and {text.length}:  characters</p>
+      <hr />
+      <p>The average time to read this : {text.split(" ").length * 2.0}</p>
+      <hr />
+      <h1>preview Text</h1>
+      <p>{text}</p>
     </div>
   )
 }
