7b967cd3b930d9e4950c395ff8187d0caf324ddf
Nitin Singh <Nitinwebdev5@gmail.com>
2023-04-11 12:26:49 +0530

    darkmodetoggle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import AboutUs from './components/AboutUs';
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div>
-      <Navbar  title="textutils" about="AboutUs"/>
-      <div className='p-5'>
-      <TextForm />
+  const [mode, setMode] = useState('light')
 
-      </div>
-        
+  const handleMode = () =>{
+    if(mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+    else{
+      setMode('dark')
+      document.body.style.backgroundColor = 'black'
+    }
+  }
+  return (
+    <>
+    <Navbar mode={mode} handleMode={handleMode}  title="TextUtils" aboutText="About Us" /> 
+    <div className="p-5">
+      <TextForm mode={mode}/>
+      {/* <AboutUs></AboutUs> */}
     </div>
+    </>
   );
 }
 
